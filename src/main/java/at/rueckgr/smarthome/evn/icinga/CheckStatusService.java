0ef59aca9d60ec2cec07034a7ec6d145524cbe65
Paul Staroch <paulchen@rueckgr.at>
2018-02-14 20:06:21 +0100

    created class CheckStatusService

---
+++ src/main/java/at/rueckgr/smarthome/evn/icinga/CheckStatusService.java
@@ -0,0 +1,101 @@
+package at.rueckgr.smarthome.evn.icinga;
+
+import at.rueckgr.smarthome.evn.remote.Room;
+import at.rueckgr.smarthome.evn.remote.SmartHomeService;
+import at.rueckgr.smarthome.evn.remote.TemperatureSettings;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CheckStatusService {
+    private static final Logger logger = LogManager.getLogger(CheckStatusService.class);
+
+    private static final int WAIT_SECONDS = 120;
+    private static final float TEMPERATURE_DIFFERENCE = .5f;
+
+    private SmartHomeService service;
+    private StatusProperties properties;
+
+    public CheckStatusService(final StatusProperties properties) {
+        this.properties = properties;
+        this.service = new SmartHomeService(properties.getUsername(), properties.getPassword());
+
+        final String sessionToken = properties.getSessionToken();
+        if (!StringUtils.isBlank(sessionToken)) {
+            service.setSessionToken(sessionToken);
+        }
+    }
+
+    public IcingaResult checkStatus() {
+        List<Problem> problems = findProblems();
+        if(!problems.isEmpty()) {
+            problems.stream().map(Problem::getRoom).distinct().forEach(this::changeTemperatureTwice);
+            problems = findProblems();
+        }
+
+        logger.info("Problems: {}", problems.toString());
+
+        properties.setSessionToken(service.getSessionToken());
+        properties.save();
+
+        final IcingaResult icingaResult = new IcingaResult();
+        if (problems.isEmpty()) {
+            icingaResult.setStatus(IcingaStatus.OK);
+            icingaResult.setText("No problems detected");
+        }
+        else {
+            icingaResult.setStatus(IcingaStatus.CRITICAL);
+            icingaResult.setText("Problem(s): " + problems.toString());
+        }
+
+        return icingaResult;
+    }
+
+    private void changeTemperatureTwice(final Room room) {
+        logger.info("Executing temperature change cycle for room {}", room.getName());
+
+        final TemperatureSettings oldSettings = room.getTemperatureSettings();
+
+        final TemperatureSettings temporaryTemperatureSettings = new TemperatureSettings(oldSettings);
+        temporaryTemperatureSettings.setTemperatureMode(TemperatureSettings.TemperatureMode.NORMAL);
+        temporaryTemperatureSettings.setAutoTemperature(oldSettings.getAutoTemperature() + TEMPERATURE_DIFFERENCE);
+        service.setTemperatureSettings(room, temporaryTemperatureSettings);
+
+        sleep(WAIT_SECONDS);
+
+        service.setTemperatureSettings(room, oldSettings);
+
+        sleep(WAIT_SECONDS);
+    }
+
+    private void sleep(int seconds) {
+        try {
+            logger.info("Sleeping {} seconds", seconds);
+            Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Problem> findProblems() {
+        return service.getState().getRooms()
+                .stream()
+                .map(this::findProblems)
+                .flatMap(Function.identity())
+                .collect(Collectors.toList());
+    }
+
+    private Stream<Problem> findProblems(final Room room) {
+        return room.getDevices()
+                .stream()
+                .filter(device -> !device.getState().isOk())
+                .map(device -> new Problem(room, device));
+    }
+}
--- src/main/java/at/rueckgr/smarthome/evn/icinga/Main.java
+++ src/main/java/at/rueckgr/smarthome/evn/icinga/Main.java
@@ -1,25 +1,12 @@
 package at.rueckgr.smarthome.evn.icinga;
 
-import at.rueckgr.smarthome.evn.remote.*;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
-
-    private final static Logger logger = LogManager.getLogger(Main.class);
-
-    private static final int WAIT_SECONDS = 120;
-    private static final float TEMPERATURE_DIFFERENCE = .5f;
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -27,34 +14,8 @@ public class Main {
         }
 
         final StatusProperties properties = new StatusProperties(args[0]);
-
-        final SmartHomeService service = new SmartHomeService(properties.getUsername(), properties.getPassword());
-
-        final String sessionToken = properties.getSessionToken();
-        if (!StringUtils.isBlank(sessionToken)) {
-            service.setSessionToken(sessionToken);
-        }
-
-        List<Problem> problems = findProblems(service);
-        if(!problems.isEmpty()) {
-            problems.stream().map(Problem::getRoom).distinct().forEach(room -> changeTemperatureTwice(service, room));
-            problems = findProblems(service);
-        }
-
-        logger.info("Problems: {}", problems.toString());
-
-        properties.setSessionToken(service.getSessionToken());
-        properties.save();
-
-        final IcingaResult icingaResult = new IcingaResult();
-        if (problems.isEmpty()) {
-            icingaResult.setStatus(IcingaStatus.OK);
-            icingaResult.setText("No problems detected");
-        }
-        else {
-            icingaResult.setStatus(IcingaStatus.CRITICAL);
-            icingaResult.setText("Problem(s): " + problems.toString());
-        }
+        final CheckStatusService service = new CheckStatusService(properties);
+        final IcingaResult icingaResult = service.checkStatus();
 
         final String statusOutputFile = properties.getStatusOutputFile();
         try (OutputStream outputStream = createOutputStream(statusOutputFile)) {
@@ -63,48 +24,6 @@ public class Main {
         catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static void changeTemperatureTwice(final SmartHomeService service, final Room room) {
-        logger.info("Executing temperature change cycle for room {}", room.getName());
-
-        final TemperatureSettings oldSettings = room.getTemperatureSettings();
-
-        final TemperatureSettings temporaryTemperatureSettings = new TemperatureSettings(oldSettings);
-        temporaryTemperatureSettings.setTemperatureMode(TemperatureSettings.TemperatureMode.NORMAL);
-        temporaryTemperatureSettings.setAutoTemperature(oldSettings.getAutoTemperature() + TEMPERATURE_DIFFERENCE);
-        service.setTemperatureSettings(room, temporaryTemperatureSettings);
-
-        sleep(WAIT_SECONDS);
-
-        service.setTemperatureSettings(room, oldSettings);
-
-        sleep(WAIT_SECONDS);
-    }
-
-    private static void sleep(int seconds) {
-        try {
-            logger.info("Sleeping {} seconds", seconds);
-            Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
-        }
-        catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static List<Problem> findProblems(SmartHomeService service) {
-        return service.getState().getRooms()
-                .stream()
-                .map(Main::findProblems)
-                .flatMap(Function.identity())
-                .collect(Collectors.toList());
-    }
-
-    private static Stream<Problem> findProblems(final Room room) {
-        return room.getDevices()
-                .stream()
-                .filter(device -> !device.getState().isOk())
-                .map(device -> new Problem(room, device));
     }
 
     private static OutputStream createOutputStream(final String statusOutputFile) throws IOException {
